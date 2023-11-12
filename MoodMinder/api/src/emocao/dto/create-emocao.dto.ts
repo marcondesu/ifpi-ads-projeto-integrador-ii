77b2389b2c6cc6de1b60006279b4be071b8e5db2
Marcos Vinícius <rkatchau@gmail.com>
2023-11-12 15:04:29 -0300

    fix: documentação DTOs finalizado

--- MoodMinder/api/src/emocao/dto/create-emocao.dto.ts
+++ MoodMinder/api/src/emocao/dto/create-emocao.dto.ts
@@ -1,9 +1,54 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CategoriaEmocao } from 'src/Categoria';
+import { Privacidade } from 'src/privacidade';
 
 export class CreateEmocaoDto {
+  @ApiProperty({
+    type: String,
+    description: 'ID do paciente',
+    required: true,
+  })
   idPaciente: string;
+
+  @ApiProperty({
+    enum: CategoriaEmocao,
+    default: CategoriaEmocao.ALEGRIA,
+    description: 'Emoção que o paciente está sentindo',
+    required: true,
+  })
   categoria: CategoriaEmocao;
+
+  @ApiProperty({
+    type: Number,
+    default: 8,
+    description: 'Intensidade da emoção (0 a 10)',
+    required: true,
+  })
   intensidade: number;
+
+  @ApiProperty({
+    type: String,
+    default: 'Me sinto feliz.',
+    description: 'Comentário sobre o que o paciente está sentindo',
+    required: false,
+  })
   descricao: string;
+
+  @ApiProperty({
+    type: String,
+    default:
+      'Cheguei meio tarde no almoço e a fila estava pequena, isso que é felicidade genuína!',
+    description: 'Comentário sobre o quê desencadeou a emoção',
+    required: true,
+  })
   contexto: string;
+
+  @ApiProperty({
+    enum: Privacidade,
+    default: Privacidade.PUBLICO,
+    description:
+      'Caso for público, o profissional que estiver acompanhando este paciente poderá ver o registro de emoção',
+    required: false,
+  })
+  privacidade: Privacidade;
 }
--- MoodMinder/api/src/feedback/dto/create-feedback.dto.ts
+++ MoodMinder/api/src/feedback/dto/create-feedback.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateFeedbackDto {
+  @ApiProperty({
+    type: String,
+    description: 'ID do acompanhamento no qual o feedback será dado',
+    required: true,
+  })
   idAcompanhamento: string;
+
+  @ApiProperty({
+    type: Number,
+    default: 10,
+    description: 'Nota de 0 a 10 para o acompanhamento',
+    required: true,
+  })
   nota: number;
+
+  @ApiProperty({
+    type: String,
+    default:
+      'Me sinto confortável para dizer o que sinto. Estou percebendo melhoras no dia a dia.',
+    description: 'Comentário sobre como está sendo o acompanhamento',
+    required: false,
+  })
   texto: string;
-  data: Date;
 }
